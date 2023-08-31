7450e37ef089f2f3b715bae4650044a734a60ac8
hemantdangeatgeir <hemant.dange@atgeirsolutions.com>
2023-08-31 16:40:26 +0530

    java 8 maven project

---
+++ src/main/java/com/marcos/pocapp/PocappApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.pocapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/pocapp/PocappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.pocapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
